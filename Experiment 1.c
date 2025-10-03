ac36fb40ee3f6193a917b30334205a9ecd79ae07
gsazure26082024 <gsazure26082024@gmail.com>
2025-10-03 21:56:47 +0530

    Initial: Desc Code

---
+++ Experiment 1.c
@@ -0,0 +1,32 @@
+//Write a program to perform addition, subtraction, multiplication, and division of
+//given numbers.
+
+
+#include <stdio.h>
+
+int main() {
+    float num1, num2;
+    float sum, difference, product, quotient;
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+    sum = num1 + num2;
+    difference = num1 - num2;
+    product = num1 * num2;
+    if (num2 != 0) {
+        quotient = num1 / num2;
+    } else {
+        quotient = 0;
+    }
+    printf("Results:\n");
+    printf("Addition = %f\n", sum);
+    printf("Subtraction = %f\n", difference);
+    printf("Multiplication = %f\n", product);
+    if (num2 != 0) {
+        printf("Division = %f\n", quotient);
+    } else {
+        printf("Division = Not possible (division by zero)\n");
+    }
+    return 0;
+}
